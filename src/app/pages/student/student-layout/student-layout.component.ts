86d7f9a3a1b7d17b5b54183ce66c2fc84adf82f3
Selvachandran R. <UT01107@unicomtic.com>
2025-03-13 15:48:14 +0530

    Added sidebar to student dashboard for navigation

--- src/app/pages/student/student-layout/student-layout.component.ts
+++ src/app/pages/student/student-layout/student-layout.component.ts
@@ -1,13 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-student-layout',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink,CommonModule],
   templateUrl: './student-layout.component.html',
   styleUrl: './student-layout.component.css'
 })
 export class StudentLayoutComponent {
 
+  isOpen = true;
+
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
+
+  constructor
+  (
+    private router:Router
+  ) {}
+
+
+  logout() {
+    // Clear local storage
+    localStorage.clear();
+
+    // Redirect to the home page
+    this.router.navigate(['/']);
+  }
 }
